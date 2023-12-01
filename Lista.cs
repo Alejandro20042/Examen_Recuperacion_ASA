9d4653dd8080f3e3fc04962f885d665011e2572e
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2023-12-01 09:17:39 -0700

    Agregar archivos de proyecto.

---
+++ Lista.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Recuperacion_ASA
+{
+    class Carros
+    {
+        public int Carro { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+
+
+        public Carros(int carro, string modelo, int año)
+        {
+            Carro = carro;
+            Modelo = modelo;
+            Año = año;
+
+        }
+    }
+
+        class Nodo
+        {
+            public Carros Datos { get; set; }
+            public Nodo Siguiente { get; set; }
+        }
+
+        class ListaEnlazada
+        {
+            private Nodo cabeza;
+
+            public void AgregarCarro(Carros carro)
+            {
+                Nodo nuevoNodo = new Nodo { Datos = carro, Siguiente = null };
+
+                if (cabeza == null)
+                {
+                    cabeza = nuevoNodo;
+                }
+                else
+                {
+                    Nodo actual = cabeza;
+                    while (actual.Siguiente != null)
+                    {
+                        actual = actual.Siguiente;
+                    }
+                    actual.Siguiente = nuevoNodo;
+                }
+            }
+
+
+        public int BuscarCarro(string nombre)
+            {
+                Nodo actual = cabeza;
+                int posicion = 0;
+
+                while (actual != null)
+                {
+                    if (actual.Datos.Modelo == nombre)
+                    {
+                        return posicion;
+                    }
+
+                    actual = actual.Siguiente;
+                    posicion++;
+                }
+
+                return -1;
+            }
+
+            public void ImprimirPorAño()
+            {
+
+
+            OrdenarPorAño();
+
+
+                Nodo actual = cabeza;
+                
+                while(actual != null)
+                {
+                    Console.WriteLine($"Nombre:{actual.Datos.Modelo},Año:{actual.Datos.Año}");
+                    actual = actual.Siguiente;
+                }
+            }
+
+            public void OrdenarPorAño()
+            {
+                if (cabeza == null || cabeza.Siguiente == null)
+                {
+                    return;
+                }
+
+                Nodo actual = cabeza;
+
+                while (actual != null)
+                {
+                    Nodo siguiente = actual.Siguiente;
+
+                    while (siguiente != null)
+                    {
+                        if (actual.Datos.Año < siguiente.Datos.Año)
+                        {
+                            Carros temp = actual.Datos;
+                            actual.Datos = siguiente.Datos;
+                            siguiente.Datos = temp;
+                        }
+
+                        siguiente = siguiente.Siguiente;
+                    }
+
+                    actual = actual.Siguiente;
+                }
+            }
+
+        }
+       
+
+    
+}
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Examen_Recuperacion_ASA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ListaEnlazada flota = new ListaEnlazada();
+            flota.AgregarCarro(new Carros(123,"Toyota",2023));
+            flota.AgregarCarro(new Carros(223, "Silverado", 2022));
+            flota.AgregarCarro(new Carros(412, "Ram", 2026));
+            flota.AgregarCarro(new Carros(764, "Chevrolet", 2018));
+            flota.AgregarCarro(new Carros(669, "Munstang", 2025));
+
+            
+            flota.ImprimirPorAño();
+
+            flota.OrdenarPorAño();
+
+            Console.WriteLine($"Se encontro el Carro{flota.BuscarCarro("Toyota")}");
+            flota.BuscarCarro("Toyota");
+        }
+    }
+}
